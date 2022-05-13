50ef35dd9378a17a2e49a8a633cba795e154f05a
Spark61 <spark61@sv-studios.net>
2022-05-13 12:03:02 +0200

    Initial commit

---
+++ src/main/java/de/spark61/DreiPlusSieben.java
@@ -0,0 +1,7 @@
+package de.spark61;
+
+public class DreiPlusSieben {
+    public static void main(final String[] args) {
+        System.out.println(3 + 7);
+    }
+}
---
+++ src/main/java/de/spark61/Multi34.java
@@ -0,0 +1,10 @@
+package de.spark61;
+
+public class Multi34 {
+    public static void main(final String[] args) {
+        final int number1 = 3;
+        final int number2 = 4;
+        final int Produkt = number1 * number2;
+        System.out.println(Produkt);
+    }
+}
